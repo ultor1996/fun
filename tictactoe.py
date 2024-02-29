1aba64fa6370dafcc195b3c94f014222389db4c1
ultor1996 <shivamrawat1237@gmail.com>
2024-02-29 16:24:45 +0100

    new changes

--- tictactoe.py
+++ tictactoe.py
@@ -4,8 +4,8 @@ for i in range(0,3):
     for j in range(0,3):
         print(t[i][j],end='\t')
     print('\n')
-user=input("choose o or x")
-pos=input("enter pos for e.g. 01,02,03,10,11,12,20,21,22")
+user=input("choose o or x\n")
+pos=input("enter pos for e.g. 01,02,03,10,11,12,20,21,22\n")
 pos=list(pos)
 pos=list(map(int,pos))
 t[pos[0]][pos[1]]=user
