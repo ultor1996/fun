b0aa52ef838ef2c72064fc9ad21715f041e21cfb
ultor1996 <shivamrawat1237@gmail.com>
2024-03-01 23:14:02 +0100

    new changes in tictactoe

--- tictactoe.py
+++ tictactoe.py
@@ -19,22 +19,27 @@ while ans == 'y':
          comp = 'o'
          break
     else :
-       ans = input(" Enter y to continue or n to exit")
+       ans = input(" Enter y to continue or n to exit\n")
        if ans == 'n' :
           exit(0)
 while win == 0 :
-    pos=input("enter pos for e.g. 00,01,02,10,11,12,20,21,22\n")
-    pos=list(pos)
-    pos=list(map(int,pos))
-    t[pos[0]][pos[1]] = user
+    upos=input("enter pos for e.g. 00,01,02,10,11,12,20,21,22\n")
+    upos=list(upos)
+    upos=list(map(int,upos))
+    t[upos[0]][upos[1]] = user
     if turn == 1 :
-        if pos[0] == 1 and pos[1]== 1 :
+        if upos[0] == 1 and upos[1]== 1 :
             cpos = random.choice(['00','02','10','12','20','22'])
             cpos = list(map(int,cpos))
             t[cpos[0]][cpos[1]] = comp
         else :
             t[1][1] = comp
+        for i in range(0, 3):
+            for j in range(0, 3):
+                print(t[i][j], end='\t')
+            print('\n')
     elif turn == 2 :
+        if t[upos[0]][upos[1]] == user :
 
 
 
