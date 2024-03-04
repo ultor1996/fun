96011cfc16694280b3a45d8e0fcc4333691d8560
ultor1996 <shivamrawat1237@gmail.com>
2024-03-04 16:43:05 +0100

    binary search done

--- binarysearch.py
+++ binarysearch.py
@@ -9,23 +9,23 @@ while i < size :
     l[i] =int(input())
     i = i+1
 for i in range (0,size-1):
-    if l[i]  > l[i+1] :
-        k= l[i+1]
-        l[i+1]=l[i]
-        l[i]=k
-print(l)
+    if l[i] > l[i+1] :
+        k = l[i+1]
+        l[i+1] = l[i]
+        l[i] = k
 s=0
-s=int(input("Enter the element to be found"))
+s=int(input("Enter the element to be found\n"))
 f=0
-l =0
+b=0
 r = int(size)-1
-while l <= r:
-    mid = l + (r-l)//2
+mid = 0
+while b <= r:
+    mid = b + (r-b)//2
     if l[mid] < s :
-        l =mid + 1
+        b =mid + 1
     elif l[mid] > s :
         r = mid - 1
-    else :
-        print("The number is at ",mid)
+    elif l[mid] == s :
+        print("The number is at ",mid+1)
         break
 
