1c36e1a221945ebd550cb2c0ce1a5b3be6225966
ultor1996 <shivamrawat1237@gmail.com>
2025-02-10 12:08:06 +0100

    automated gradient calaculation in backporpogation

--- neural netowrk/backpropogation.py
+++ neural netowrk/backpropogation.py
@@ -17,6 +17,7 @@ class Value:
     def __init__(self, data, _children=(), _op='', label =''):
         self.data = data
         self.grad=0.0
+        self._backward =  lambda: None
         self._prev = set(_children)
         self._op = _op
         self.label=label
@@ -25,14 +26,51 @@ class Value:
         return f"value(data={self.data})"  # f is fomratted print where you can print varibale isnide a given text  statement, also its an automatic print statemnet
 
     def __add__(self, other):
-        return Value(self.data + other.data, (self, other),
+        out= Value(self.data + other.data, (self, other),
                      '+')  # add function show how to add the class objects and their elements individually
-
+        def _backward():
+            self.grad+=1.0*out.grad #here we are defining the gradient to be calculated automatiacally via the code rather than puttung invia hands
+            other.grad+=1.0*out.grad# so for a node say t, t.grad=do/dn * dn/dt_n * dt_n/dt_n1 * ....... *dt_1/dt = t1.grad( everything except the last term or one can say a kind of global derivative) * dt_1/dt
+                                   #= t1.grad * 1 ( as t is in t1 via the '+' operand), here t1.grad=out.grad
+        out._backward=_backward
+        return out
     def __mult__(self, other):
-        return Value(self.data * other.data, (self, other), '*')
+       out = Value(self.data * other.data, (self, other), '*')
+
+       def _backward():
+           self.grad += other.data* out.grad  # here we are defining the gradient to be calculated automatiacally via the code rather than puttung invia hands
+           other.grad += self.data * out.grad  # so for a node say t, t.grad=do/dn * dn/dt_n * dt_n/dt_n1 * ....... *dt_1/dt = t1.grad( everything except the last term or one can say a kind of global derivative) * dt_1/dt
+           # = t1.grad * the other term (for e.g. if the dependency of t1 on t is as t1= a * t, then dt_1/dt=a), here t1.grad=out.grad
+
+       out._backward = _backward
+       return out
 
     def tanh(self):
-        return Value ((math.exp(2*self.data)-1)/(math.exp(2*self.data)+1),(self,),'tanh')
+        x=self.data
+        t=(math.exp(2*x)-1)/(math.exp(2*x)+1)
+        out =  Value (t,(self,),'tanh')
+
+        def _backward():
+            self.grad += (1-t**2)* out.grad  # here we are defining the gradient to be calculated automatiacally via the code rather than puttung invia hands
+            # so for a node say t, t.grad=do/dn * dn/dt_n * dt_n/dt_n1 * ....... *dt_1/dt = t1.grad( everything except the last term or one can say a kind of global derivative) * dt_1/dt
+            # = t1.grad * (1-data**2) (dt_1/dt=1-t.data**2), here t1.grad=out.grad
+        out._backward = _backward
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build_topo(v): # this makes a list of the nodes going from last nodes towards earleuir ones in topo
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        self.grad=1.0
+        for node in reversed(topo): # then in this we go through the topo in opposite direction that is from 1st node to last
+            node._backward()# to fill in the gradients going backwards to the first node
 
 
 a = Value(2.0,label='a')
@@ -50,10 +88,10 @@ def trace(root):
     def build(v): # build is called with d via trace in draw_dot
         if v not in nodes:
             nodes.add(v)
-            print("nodes",nodes)
+            #print("nodes",nodes)
             for child in v._prev:
                 edges.add((child, v))
-                print("edges",edges)
+             #   print("edges",edges)
                 build(child)
 
     build(root)
@@ -143,9 +181,24 @@ x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1*w1 + x2*w2'
 n = x1w1x2w2 + b; n.label = 'n'# the collective input of the previous tow neurons to the current neuron/node
 #draw_dot(n) # this shows the node contribution of the previous nodes/ neurons to the current one withput activation fcntion uncomment to plot
 o=n.tanh(); o.label='o'
-o.grad=1.0# again doing the derivative things
-n.grad= 0.5#do/dn=1-tanh(n)**2=1-o**2=1-0.7071**2
-b.grad=0.5#do/db=do/dn * dn/db=do/dn * 1
-x1w1x2w2.grad=0.5#do/d(x1w1x2w2)=do/dn * dn/d(x1w1x2w2)= do/dn * 1
-x2w2.grad=0.5#do/d(x2w2)=do/dn * dn/d(x1w1+x2w2) * d(x1w1x2w2)/d(x2w2)=do/dn * 1 *1
-draw_dot(o)#this shows the node contribution of the previous nodes/ neurons to the current one with activation fcntion uncomment to plot
+#o.grad=1.0# again doing the derivative things
+#n.grad= 0.5#do/dn=1-tanh(n)**2=1-o**2=1-0.7071**2
+#b.grad=0.5#do/db=do/dn * dn/db=do/dn * 1
+#x1w1x2w2.grad=0.5#do/d(x1w1x2w2)=do/dn * dn/d(x1w1x2w2)= do/dn * 1
+#x2w2.grad=0.5#do/d(x2w2)=do/dn * dn/d(x1w1+x2w2) * d(x1w1x2w2)/d(x2w2)=do/dn * 1 *1
+#x1w1.grad=0.5#do/d(x1w1)=do/dn * dn/d(x1w1+x2w2) * d(x1w1x2w2)/d(x1w1)=do/dn * 1 *1
+#w1.grad= 1.0#do/d(w1)=do/dn * dn/d(x1w1+x2w2) * d(x1w1+x2w2)/d(x1w1) *d(x1w1)/d(w1)=do/dn *1 *1 *x1
+#w2.grad=0.0 #do/d(w1)=do/dn * dn/d(x1w1+x2w2) * d(x1w1+x2w2)/d(x2w2) *d(x2w2)/d(w2)=do/dn *1 *1 *x2
+#x1.grad= -1.5#do/d(w1)=do/dn * dn/d(x1w1+x2w2) * d(x1w1+x2w2)/d(x1w1) *d(x1w1)/d(x1)=do/dn *1 *1 *w1
+#x2.grad=0.5 #do/d(w1)=do/dn * dn/d(x1w1+x2w2) * d(x1w1+x2w2)/d(x2w2) *d(x2w2)/d(x2)=do/dn *1 *1 *w2
+#draw_dot(o)#this shows the node contribution of the previous nodes/ neurons to the current one with activation fcntion uncomment to plot
+# now we dont have to insert the derivative manually so we udodated the class and now we can  do it via funciotn even though the first .grad still needs to be initlaized
+#o._backward()
+#n._backward()
+#b._backward()# nothing will happen as b is not connected backward to any other node
+#x1w1x2w2._backward()
+#x1w1._backward()
+#x2w2._backward()
+# now instead of calling backward again and again we can build toplogical sort done in value class, to do it automatically
+o.backward()
+draw_dot(o)
